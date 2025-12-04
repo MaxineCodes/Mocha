983759954c65ce01fb9158187e43bab99d71314e
Maxine <89635193+MaxineCodes@users.noreply.github.com>
2025-12-04 17:26:30 +0100

    shader class basic compiling - current commit wont compile

--- src/core/Shader.cpp
+++ src/core/Shader.cpp
@@ -4,4 +4,64 @@
 
 #include "Shader.h"
 
-namespace Mocha {} // Mocha
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <glad/glad.h>
+#include "../interface/logger.h"
+
+namespace Mocha
+{
+    Shader::Shader(const char *vertexPath, const char *fragmentPath)
+    {
+        const char* vertexShaderCode = readFileFromPath(vertexPath);
+        const char* fragmentShaderCode = readFileFromPath(fragmentPath);
+
+        unsigned int vertexShader, fragmentShader;
+        int success;
+        char infoLog[512];
+
+        // vertex Shader
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderCode, NULL);
+        glCompileShader(vertexShader);
+
+        // print compile errors if any
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if(!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            logger::logGLError("ERROR::SHADER::VERTEX::COMPILATION_FAILED");
+            logger::logGLError(infoLog);
+        };
+    }
+
+    const char * Shader::readFileFromPath(const char *path)
+    {
+        std::string rawShaderCode;
+        std::ifstream shaderFile;
+
+        // ensure ifstream objects can throw exceptions:
+        shaderFile.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+
+        try
+        {
+            // open files
+            shaderFile.open(path);
+            std::stringstream shaderStream;
+            // read file's buffer contents into streams
+           shaderStream << rawShaderCode.rdbuf();
+            // close file handlers
+            shaderFile.close();
+            // convert stream into string
+            rawShaderCode = shaderStream.str();
+        }
+        catch (std::ifstream::failure e)
+        {
+            logger::logGLError("ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ");
+        }
+
+        return rawShaderCode.c_str();
+    }
+} // Mocha
--- src/core/Shader.h
+++ src/core/Shader.h
@@ -4,10 +4,34 @@
 
 #ifndef MOCHA_SHADER_H
 #define MOCHA_SHADER_H
+#include <string>
+
+#include "imgui_impl_opengl3_loader.h"
 
 namespace Mocha
 {
-    class Shader {};
+    class Shader
+    {
+    public:
+        unsigned int programID;
+
+        Shader(const char* vertexPath, const char* fragmentPath);
+        ~Shader();
+
+        GLuint program;
+        GLuint vertexShader;
+        GLuint fragmentShader;
+
+        void use();
+        void setBool(const std::string& name, bool value) const;
+        void setInt(const std::string& name, int value) const;
+        void setFloat(const std::string& name, float value) const;
+
+    private:
+        const char* readFileFromPath(const char* path);
+
+
+    };
 } // Mocha
 
 #endif //MOCHA_SHADER_H
