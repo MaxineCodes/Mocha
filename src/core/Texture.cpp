f70c6ca22bb86fc27fc3d23caa69806d86ac1107
Maxine <89635193+MaxineCodes@users.noreply.github.com>
2025-12-05 16:31:40 +0100

    added texture class and added some textures to play with

---
+++ src/core/Texture.cpp
@@ -0,0 +1,7 @@
+//
+// Created by maxin on 05/12/2025.
+//
+
+#include "Texture.h"
+
+namespace Mocha {} // Mocha
---
+++ src/core/Texture.h
@@ -0,0 +1,23 @@
+//
+// Created by maxin on 05/12/2025.
+//
+
+#ifndef MOCHA_TEXTURE_H
+#define MOCHA_TEXTURE_H
+#include <glad/glad.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+namespace Mocha
+{
+    struct Texture
+    {
+        GLuint textureID;
+        int width, height, channels;
+
+        Texture(const char* path);
+    };
+} // Mocha
+
+#endif //MOCHA_TEXTURE_H
