56e6dceda47c114cd0848e71bcfbe7187431c3fd
MaxineCodes <MaxineCodes@github.com>
2026-01-27 14:54:09 +0100

    testing github auth

--- src/core/rendering.h
+++ src/core/rendering.h
@@ -14,7 +14,7 @@ namespace Mocha
     inline const char* vertexShaderSource = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
-    layout (location = 1) in vec3 aColor;
+    layout (location = 1) in vec3 aColor;   
 
     out vec3 fragColor;
 
