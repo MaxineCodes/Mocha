a92d1f2465b92afc46ee06ed40b37e9c42d264fc
Maxine Meijboom <MaxineCodes@users.noreply.github.com>
2026-03-18 21:42:51 +0100

    camera class

---
+++ src/core/camera/Camera.h
@@ -0,0 +1,97 @@
+//
+// Created by maxine on 18/03/2026.
+//
+
+/*
+ *  Camera.h
+ *
+ *  For now, the camera implementation as seen in learnopengl.com
+ *
+ */
+
+#ifndef MOCHA_CAMERA_H
+#define MOCHA_CAMERA_H
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+namespace Mocha
+{
+    // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+    enum Camera_Movement
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    };
+    // Default camera values
+    const float YAW         = -90.0f;
+    const float PITCH       =  0.0f;
+    const float SPEED       =  2.5f;
+    const float SENSITIVITY =  0.1f;
+    const float ZOOM        =  45.0f;
+
+
+    class Camera
+    {
+    public:
+        // camera Attributes
+        glm::vec3 Position;
+        glm::vec3 Front;
+        glm::vec3 Up;
+        glm::vec3 Right;
+        glm::vec3 WorldUp;
+        // euler Angles
+        float Yaw;
+        float Pitch;
+        // camera options
+        float MovementSpeed;
+        float MouseSensitivity;
+        float Zoom;
+
+        // constructor with vectors
+        Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+            glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+            float yaw = YAW, float pitch = PITCH)
+            : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+            MovementSpeed(SPEED),
+            MouseSensitivity(SENSITIVITY),
+            Zoom(ZOOM)
+        {
+            Position = position;
+            WorldUp = up;
+            Yaw = yaw;
+            Pitch = pitch;
+            updateCameraVectors();
+        }
+        // constructor with scalar values
+        Camera(float posX, float posY, float posZ,
+            float upX, float upY, float upZ,
+            float yaw, float pitch)
+            : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+            MovementSpeed(SPEED),
+            MouseSensitivity(SENSITIVITY),
+            Zoom(ZOOM)
+        {
+            Position = glm::vec3(posX, posY, posZ);
+            WorldUp = glm::vec3(upX, upY, upZ);
+            Yaw = yaw;
+            Pitch = pitch;
+            updateCameraVectors();
+        }
+        // returns the view matrix calculated using Euler Angles and the LookAt Matrix
+
+        glm::mat4 GetViewMatrix();
+        void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+        void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true);
+        void ProcessMouseScroll(float yoffset);
+
+    private:
+        void updateCameraVectors();
+
+
+    };
+} // Mocha
+
+#endif //MOCHA_CAMERA_H
